599ee727c555df9c431623c7c07eb03bab359883
secretpray <Renovatio100>
2020-12-24 21:40:06 +0200

    Lesson 1

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it "is not valid without attributes" do
+    expect(Answer.new).to_not be_valid
+  end
+
+  describe "Associations" do
+    it { should belong_to(:question).without_validating_presence }
+  end
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it "is not valid without attributes" do
+    expect(Question.new).to_not be_valid
+  end
+
+  describe "Uniqueness of title" do
+    before do
+      # @question = Question.new(title: "Is Ruby..?", body: "Is Ruby an interpreted programming language?")
+      @question = FactoryBot.create(:question, title: "Is Ruby..?", body: "Is Ruby an interpreted programming language?")
+    end
+
+    describe "When question title is not unique" do
+      before do
+        question_with_same_title = @question.dup
+        question_with_same_title.save
+      end
+
+      it { should_not be_valid }
+
+    end
+  end
+
+  describe "Associations" do
+    it { should have_many(:answers).without_validating_presence }
+  end
+
 end
