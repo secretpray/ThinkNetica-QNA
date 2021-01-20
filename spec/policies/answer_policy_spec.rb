f996e7e85760ae2184266827d169e8906cbdd80c
secretpray <Renovatio100>
2021-01-20 13:04:35 +0200

    Some fixes

--- spec/policies/answer_policy_spec.rb
+++ spec/policies/answer_policy_spec.rb
@@ -5,23 +5,7 @@ RSpec.describe AnswerPolicy, type: :policy do
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    # pending "add some examples to (or delete) #{__FILE__}"
   end
 end
