33cbb82244c3b1c3223f3d123201a9e4efaa2071
secretpray <Renovatio100>
2021-02-01 10:24:46 +0200

    Rewards feature ref#2

--- db/migrate/20210201055924_create_rewards.rb
+++ db/migrate/20210201055924_create_rewards.rb
@@ -1,7 +1,7 @@
 class CreateRewards < ActiveRecord::Migration[6.1]
   def change
     create_table :rewards do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :question, null: false, foreign_key: true
       t.string :name, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_143446) do
+ActiveRecord::Schema.define(version: 2021_02_01_055924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_143446) do
   end
 
   create_table "rewards", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.bigint "question_id", null: false
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
