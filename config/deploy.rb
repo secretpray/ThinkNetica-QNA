7a345780c87f156c95f02c8f2327239a5a652ccd
secretpray <Renovatio100>
2021-07-21 14:37:00 +0300

    Reedit config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,9 @@ set :deploy_user, 'deployer'
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
 
-SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
-SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
+# SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+# SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
+
 set :pty,  false
-set :init_system, :systemd
-set :service_unit_name, "sidekiq.service"
+# set :init_system, :systemd
+# set :service_unit_name, "sidekiq.service"
