898d3990b566f0638f189d8737c5613618a88e0f
secretpray <Renovatio100>
2021-07-23 13:11:40 +0300

    Gmail added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_cable.url = "ws://localhost:3000/cable"
+
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
@@ -76,7 +79,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(Rails.env.to_sym, :gmail, :user_name),
+    password:             Rails.application.credentials.dig(Rails.env.to_sym, :gmail, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
+  # config.action_mailer.delivery_method = :letter_opener
   # config.action_mailer.delivery_method = :letter_opener_web"
   config.action_mailer.perform_deliveries = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "qna_production"
 
+  # config.action_mailer.default_url_options = {
+  #   ENV['POW_DOMAINS'].present? ? 'my-app.dev' : 'localhost'
+  # }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://164.90.163.230' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.dig(Rails.env.to_sym, :gmail, :user_name),
+    password:             Rails.application.credentials.dig(Rails.env.to_sym, :gmail, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
