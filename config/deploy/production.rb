a42fa7ae141b7c401b294149e27c0c73459fa996
secretpray <Renovatio100>
2021-07-30 07:08:29 +0300

    return to passenger

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "164.90.163.230", user: "deployer", roles: %w{app db web}, primary: true
-server "167.99.37.58", user: "deployer", roles: %w{app db web}, primary: true
+# server "167.99.37.58", user: "deployer", roles: %w{app db web}, primary: true
+server "178.62.216.118", user: "deployer", roles: %w{app db web}, primary: true
+
 set :rail_env, :production
 
 # Global options
