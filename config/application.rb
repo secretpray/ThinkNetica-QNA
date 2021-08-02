d426f3573bc22dd6f9f209250fa92649e6ea249b
secretpray <Renovatio100>
2021-08-02 18:49:44 +0300

    Cache qna show with redis_store on application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Qna
     config.active_job.queue_adapter = :sidekiq
 
     # config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    config.cache_store = :redis_cache_store, { url: "redis://localhost:6379/0/cache", expires_in: 90.minutes }
 
     config.generators do |g|
       g.test_framework :rspec,
